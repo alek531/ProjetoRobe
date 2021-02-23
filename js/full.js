cd79187de3bb17bf6330714232167b3879e2e8fc
alex gerboni <gerboni2019@gmail.com>
2021-02-22 22:39:09 -0300

    robeok

---
+++ js/full.js
@@ -0,0 +1,10 @@
+$(function() {
+	var effects = 'animated bounce';
+	var effectsEnd = 'animationed oAnimationEnd mozAnimationEnd webkitAnimationEnd';
+
+	$('a.button').hover(function(){
+		$(this).addClass(effects).one(effectsEnd,function(){
+			$(this).removeClass(effects);
+		});
+	});
+});
